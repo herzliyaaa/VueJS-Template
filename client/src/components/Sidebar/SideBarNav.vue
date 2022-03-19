2976d56f5f772e80fc50b5c15557302ed8d22139
herzliyaaa <herzliabarangan@gmail.com>
2022-03-19 20:24:11 +0800

    added tailwind for styling

---
+++ client/src/components/Sidebar/SideBarNav.vue
@@ -0,0 +1,20 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav class="flex justify-end items-center bg-indigo-500 w-screen h-14">
+    <div class="flex p-2 text-md text-white font-bold ">
+      <RouterLink to="/">HOME</RouterLink>
+    </div>
+
+<div class="flex p-2 text-md text-white font-bold ">
+      <RouterLink to="/">LINK</RouterLink>
+    </div>
+
+    <div class="flex p-2 text-md text-white font-bold ">
+      <RouterLink to="/about">ABOUT</RouterLink>
+    </div>
+  </nav>
+</template>
+
---
+++ client/tailwind.config.js
@@ -0,0 +1,17 @@
+
+
+module.exports = {
+  mode: 'jit',
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './src/*.{vue,js,ts,jsx,tsx}',
+    './src/components/**/*.{vue,js,ts,jsx,tsx}'
+
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
